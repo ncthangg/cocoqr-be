f6684f745aec95edfbc7ef3a929ba4283d52b6f2
ncthangg <ncthang2206@gmail.com>
2026-03-03 23:40:24 +0700

    fix Program.cs

--- MyWallet.API/Program.cs
+++ MyWallet.API/Program.cs
@@ -1,21 +1,15 @@
-﻿using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MyWallet.API.DependencyInjection;
+using MyWallet.API.Extensions;
 using MyWallet.API.Middlewares;
 using MyWallet.Application.DependencyInjection;
 using MyWallet.Infrastructure.DependencyInjection;
 using MyWallet.Infrastructure.Persistence.MyDbContext;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders();
-builder.Logging.AddSimpleConsole(options =>
-{
-    options.TimestampFormat = "HH:mm:ss ";
-    options.SingleLine = true;
-});
-Console.OutputEncoding = Encoding.UTF8;
+builder.AddCustomLogging();
+builder.Services.AddForwardedHeadersConfig();
 
 // Add services to the container.
 builder.Services.AddPresentation(builder.Configuration);
@@ -27,36 +21,36 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var logger = services.GetRequiredService<ILogger<Program>>();
 
-    try
-    {
-        var db = services.GetRequiredService<MyWalletDbContext>();
-
-        logger.LogInformation("Checking for pending database migrations...");
-
-        await db.Database.MigrateAsync();
-
-        logger.LogInformation("Database migrations completed successfully");
-
-    }
-    catch (Exception ex)
-    {
-        logger.LogError(ex, "An error occurred during database initialization");
-
-        // throw; 
-    }
-}
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var logger = services.GetRequiredService<ILogger<Program>>();
+
+        try
+        {
+            var db = services.GetRequiredService<MyWalletDbContext>();
+
+            logger.LogInformation("Checking for pending database migrations...");
+
+            await db.Database.MigrateAsync();
+
+            logger.LogInformation("Database migrations completed successfully");
+
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred during database initialization");
+
+            // throw; 
+        }
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Portfolios API V1")
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Portfolios API - DEVELOP")
     );
 }
 
@@ -64,23 +58,11 @@ if (app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Portfolios API V1")
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Portfolios API - STAGING")
     );
 }
 
-var forwardOptions = new ForwardedHeadersOptions
-{
-    ForwardedHeaders =
-        ForwardedHeaders.XForwardedFor |
-        ForwardedHeaders.XForwardedProto |
-        ForwardedHeaders.XForwardedHost
-};
-
-// 🔥 CỰC KỲ QUAN TRỌNG
-forwardOptions.KnownNetworks.Clear();
-forwardOptions.KnownProxies.Clear();
-
-app.UseForwardedHeaders(forwardOptions);
+app.UseCustomForwardedHeaders();
 
 app.UseHttpsRedirection();
 
