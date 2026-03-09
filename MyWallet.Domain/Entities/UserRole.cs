2e08294babe8c33b4edefdc3e6ab1dee87ea2f31
ncthangg <ncthang2206@gmail.com>
2026-03-10 00:51:44 +0700

    update UserRole entity

--- MyWallet.Domain/Entities/UserRole.cs
+++ MyWallet.Domain/Entities/UserRole.cs
@@ -3,7 +3,7 @@
     public class UserRole : BaseEntity
     {
         public required Guid UserId { get; set; }
-        public required Guid RoleId { get; set; }
+        public required Guid? RoleId { get; set; }
 
         public virtual User? User { get; set; }
         public virtual Role? Role { get; set; }
