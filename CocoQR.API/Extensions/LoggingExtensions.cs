9ff93067bef58327bad4fdaadea446c1c3382c02
ncthangg <ncthang2206@gmail.com>
2026-03-27 14:14:31 +0700

    add and setting SeriLog

--- CocoQR.API/Extensions/LoggingExtensions.cs
+++ CocoQR.API/Extensions/LoggingExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Serilog;
+using System.Text;
 
 namespace CocoQR.API.Extensions
 {
@@ -6,16 +7,39 @@ namespace CocoQR.API.Extensions
     {
         public static WebApplicationBuilder AddCustomLogging(this WebApplicationBuilder builder)
         {
-            builder.Logging.ClearProviders();
-            builder.Logging.AddSimpleConsole(options =>
+            var env = builder.Environment;
+
+            if (env.IsDevelopment())
             {
-                options.TimestampFormat = "HH:mm:ss ";
-                options.SingleLine = true;
-            });
+                builder.Logging.ClearProviders();
+                builder.Logging.AddSimpleConsole(options =>
+                {
+                    options.TimestampFormat = "HH:mm:ss ";
+                    options.SingleLine = true;
+                });
 
-            Console.OutputEncoding = Encoding.UTF8;
+                Console.OutputEncoding = Encoding.UTF8;
+                return builder;
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Warning()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .WriteTo.File(
+                        "logs/log-.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 7,
+                        encoding: Encoding.UTF8,
+                        outputTemplate:
+                        "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+                    )
+                    .CreateLogger();
 
-            return builder;
+                builder.Host.UseSerilog();
+                return builder;
+            }
         }
     }
 }
